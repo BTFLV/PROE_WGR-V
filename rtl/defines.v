909c773d300e6e0612009e7a17d7433342798695
Twobsel <165266129+Twobsel@users.noreply.github.com>
2025-03-27 15:31:02 +0100

    Update defines.v

--- rtl/defines.v
+++ rtl/defines.v
@@ -11,49 +11,51 @@
  */
 
 /**
+ * module defines
  * @brief Gibt die Taktfrequenz des Systems in Hz an, z. B. 12 MHz.
- * @macro CLK_FREQ
+ * @parameter CLK_FREQ
  */
 `define CLK_FREQ       12_000_000
 
 /**
+ * @module defines
  * @brief Berechnet den Teilerwert für eine angegebene Baudrate. 
  *        Beispiel: `BAUD_DIV(115200)` bei 12 MHz ergibt den passenden Teilwert.
- * @macro BAUD_DIV(Baud)
+ * @parameter BAUD_DIV(Baud)
  */
 `define BAUD_DIV(Baud) ((`CLK_FREQ / Baud) - 1)
 
 /**
  * @brief Aktiviert (falls definiert) den Betrieb als 32-Bit RISC-V-Kern.
- * @macro RV32I
+ * @parameter RV32I
  */
 `define RV32I
 
 /**
  * @brief Aktiviert (falls definiert) die Verwendung des FRAM-Speichers statt internem RAM.
- * @macro FRAM_MEMORY
+ * @parameter FRAM_MEMORY
  */
 //`define FRAM_MEMORY
 
 /**
  * @brief Legt die Tiefe (Anzahl Einträge) für die TX- und RX-FIFOs des UART fest.
- * @macro UART_FIFO_TX_DEPTH
- * @macro UART_FIFO_RX_DEPTH
+ * @parameter UART_FIFO_TX_DEPTH
+ * @parameter UART_FIFO_RX_DEPTH
  */
 `define UART_FIFO_TX_DEPTH 4
 `define UART_FIFO_RX_DEPTH 4
 
 /**
  * @brief Legt die Tiefe (Anzahl Einträge) für die TX- und RX-FIFOs des SPI fest.
- * @macro SPI_FIFO_TX_DEPTH
- * @macro SPI_FIFO_RX_DEPTH
+ * @parameter SPI_FIFO_TX_DEPTH
+ * @parameter SPI_FIFO_RX_DEPTH
  */
 `define SPI_FIFO_TX_DEPTH 4
 `define SPI_FIFO_RX_DEPTH 4
 
 /**
  * @brief Schalter zum bedingten Einbinden der jeweiligen Peripheriemodule.
- * @macro INCLUDE_DEBUG, INCLUDE_UART, INCLUDE_TIME, ...
+ * @parameter INCLUDE_DEBUG, INCLUDE_UART, INCLUDE_TIME, ...
  */
 `define INCLUDE_DEBUG
 `define INCLUDE_UART
